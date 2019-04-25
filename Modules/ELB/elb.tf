96a08cf65d6db27383628d2eddba802e089c8f38
Manuj <mndubey91@gmail.com>
2019-04-25 21:45:47 +0530

    Update elb.tf

--- Modules/ELB/elb.tf
+++ Modules/ELB/elb.tf
@@ -21,6 +21,7 @@ resource "aws_lb_target_group" "tg" {
   port     = "${var.port}"
   protocol = "${var.protocol}"
   vpc_id   = "${var.vpc_id}"
+  healthy_threshold = 2
 #  stickiness = []
 
  tags {
